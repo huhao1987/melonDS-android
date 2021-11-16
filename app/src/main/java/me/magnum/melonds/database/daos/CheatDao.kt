3c6501917d86137529a12ed1791a82295a820c52
Hao <huhao1987@gmail.com>
2021-11-17 00:26:37 +1100

    Fix bug

--- app/src/main/java/me/magnum/melonds/database/daos/CheatDao.kt
+++ app/src/main/java/me/magnum/melonds/database/daos/CheatDao.kt
@@ -1,9 +1,6 @@
 package me.magnum.melonds.database.daos
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import io.reactivex.Single
 import me.magnum.melonds.database.entities.CheatEntity
 import me.magnum.melonds.database.entities.CheatStatusUpdate
@@ -17,6 +14,7 @@ interface CheatDao {
     @Insert
     fun insertCheats(cheatEntities: List<CheatEntity>): List<Long>
 
+    @SuppressWarnings(RoomWarnings.CURSOR_MISMATCH)
     @Query("SELECT * FROM game LEFT JOIN cheat_folder ON game.id = cheat_folder.game_id LEFT JOIN cheat ON cheat_folder.id = cheat.cheat_folder_id WHERE game.game_code = :gameCode AND (game.game_checksum IS NULL OR game.game_checksum = :gameChecksum) AND cheat.enabled = 1")
     fun getEnabledRomCheats(gameCode: String, gameChecksum: String): Single<List<Cheat>>
 
